6e3ccb5162417b04f0759c29eb41d373c1214ddb
Sanjana Ayshi <sanjana.ayshi92@gmail.com>
2024-02-08 16:21:47 +0600

    render issue fix

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://markit-illf.onrender.com', 'https://*.127.0.0.1']
 
 # #cors fix
 # CORS_ORIGIN_ALLOW_ALL = True
